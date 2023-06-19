17a5b74b75e8a82d8541bec1dc09cbcc5222152a
PawelDowgialloe <pawel.dowgiallo.84@gmail.com>
2023-06-19 20:13:13 +0200

    Blok13 CSS nex steps

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -6,6 +6,9 @@ import { TextFundamentsMetaData } from './TextFundaments/router-data';
 import { GoogleFontsMetaData } from './GoogleFonts/router-data';
 import { Blok12BCMetaData } from './Blok12BC/router-data';
 import { Block12ListsMetaData } from './Block12Lists/router-data';
+import { BoxModel } from './BoxModel/BoxModel';
+import { BoxModelMetaData } from './BoxModel/router-data';
+
 
 export const blockRouterMetaData = [
   HTMLAndCSSMetaData,
@@ -14,6 +17,7 @@ export const blockRouterMetaData = [
   GoogleFontsMetaData,
   Blok12BCMetaData,
   Block12ListsMetaData,
+  BoxModelMetaData,
 
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
