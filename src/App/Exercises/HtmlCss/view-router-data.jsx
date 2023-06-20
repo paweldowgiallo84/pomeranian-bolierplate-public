1d5c91320f991c24c2f0e1cfd3d06ba7ee4b4fc0
PawelDowgialloe <pawel.dowgiallo.84@gmail.com>
2023-06-20 21:58:49 +0200

    Block 14 css table styling

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -6,8 +6,9 @@ import { TextFundamentsMetaData } from './TextFundaments/router-data';
 import { GoogleFontsMetaData } from './GoogleFonts/router-data';
 import { Blok12BCMetaData } from './Blok12BC/router-data';
 import { Block12ListsMetaData } from './Block12Lists/router-data';
-import { BoxModel } from './BoxModel/BoxModel';
 import { BoxModelMetaData } from './BoxModel/router-data';
+import { TablesMetaData } from './Tables/router-data';
+import { Tables02RatingMetaData } from './Tables02Rating/router-data';
 
 export const blockRouterMetaData = [
   HTMLAndCSSMetaData,
@@ -17,5 +18,7 @@ export const blockRouterMetaData = [
   Blok12BCMetaData,
   Block12ListsMetaData,
   BoxModelMetaData,
+  TablesMetaData,
+  Tables02RatingMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
