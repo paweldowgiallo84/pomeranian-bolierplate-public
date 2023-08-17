6fd7486305286d46291fb1b9a16f11306c3457a5
PawelDowgialloe <pawel.dowgiallo.84@gmail.com>
2023-08-17 13:33:28 +0200

    memo game "fullscreen mode"

--- src/App/Exercises/Js/MemoGame/index.jsx
+++ src/App/Exercises/Js/MemoGame/index.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TitleAndDescription from './components/TitleAndDescription/TitleAndDescription';
 import { Memo } from './components/Memo';
 
 import './styles.css';
 
 export const MemoGame = () => {
+  const [showFullscreen, setShowFullscreen] = useState(false);
+
+  const openFullscreenComponent = () => {
+    setShowFullscreen(true);
+  };
+
+  const closeFullscreenComponent = () => {
+    setShowFullscreen(false);
+  };
+
   return (
     <>
       <div>
+        <div>{!showFullscreen && <TitleAndDescription />}</div>
         <div>
-          <TitleAndDescription />
+          {!showFullscreen && (
+            <button className="btn memo__btn" onClick={openFullscreenComponent}>
+              URUCHOM GRE
+            </button>
+          )}
         </div>
-        <Memo />
+        {showFullscreen && (
+          <div className="memo__game-fullscreen">
+            <div className="memo__game-content">
+              <Memo />
+              <div className="close__btn">
+                <button
+                  className="btn memo__btn"
+                  onClick={closeFullscreenComponent}
+                >
+                  ZAMKNIJ GRE
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
